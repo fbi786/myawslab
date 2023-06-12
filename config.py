3381a85b23ea6eb2391886f5c835467cb7288f15
kapil tanwar <118336187+fbi786@users.noreply.github.com>
2023-06-12 13:16:35 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.c1qyngtp5glg.ap-south-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "12345678"
+customdb = "employee"
+custombucket = "magicemployee"
+customregion = "ap-south-1"
